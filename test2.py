a9e8e8ffaab130339b341855cfe961d688df6676
navideyar <nvd.sfy@gmail.com>
2025-02-12 16:03:30 +0330

    add three pages
    3 pages as home, about and contact by template in seprete apps

--- test2.py
+++
@@ -1,6 +0,0 @@
-password = input("Enter your password here: ")
-
-if ( password.count ("") >= 9 ) and ( "@" or "!" in password)  :
-    print ( "password ok" )
-else :
-    print ( "password too weak, return" )
