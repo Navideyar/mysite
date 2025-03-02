4abd67ddec97618d51e1dd6b8df869af31acad57
navideyar <nvd.sfy@gmail.com>
2025-03-02 18:08:35 +0330

    Adding a category field

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # Register your models here.
 
 
-@admin.register(Post)
+
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     empty_value_display = "-empty-"
-    fields = ["image","author", "title", "content", "status"]
-    list_display = ["title", "author" , "status", "created_date", "updated_date", "counted_views"]
+    list_display = ["title", "author" , "counted_views", "status", "published_date" , "created_date"]
     list_filter = ["author", "status", "created_date", "updated_date"]
     search_fields = ["title", "content", "author__username"]
+    
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
-
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+        
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE , null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     image = models.ImageField(upload_to="blog/", default="blog/default.jpg")
-    # tag
-    # tag = models.CharField(max_length=255)
-    # category
-    # ategory = models.CharField(max_length=255)
-    # counted_view
+    category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateField(null=True)
@@ -26,3 +25,5 @@ class Post(models.Model):
         
     def __str__(self):
         return f"{self.id} - {self.title}"
+
+
