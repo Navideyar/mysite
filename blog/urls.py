08b6c86dd179b68ed1e71f49acf7d798eceaa50b
navideyar <nvd.sfy@gmail.com>
2025-03-11 21:56:31 +0330

    Filter search form
    Filtering posts based on the search form

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import blog_view, blog_single, test
+from blog.views import blog_view, blog_single, test, blog_category
 
 app_name = "blog"
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path("", blog_view, name="index"),
     path("<int:pid>", blog_single, name="single"),
     path("test", test, name="test"),
+    path("category/<str:cat_name>/", blog_category, name="category"),
+    path("search/", blog_search, name="search"),
+    path("author/<str:author_username>/", blog_view, name="author"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post
+from blog.models import Post, Category
 
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request, cat_name=None, author_username=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
+    if author_username:
+        posts = posts.filter(author__username=author_username)
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
 
@@ -18,4 +22,19 @@ def blog_single(request, pid):
 
 def test(request):
     return render(request, "test.html")
+
+
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {"posts": posts}
+    return render(request, "blog/blog-home.html", context)
    
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == "POST":
+        search_query = request.POST.get("search")
+        posts = posts.filter(title__icontains=search_query)
+    context = {"posts": posts}
+    return render(request, "blog/blog-home.html", context)
+
